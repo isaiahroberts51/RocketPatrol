0dba2c35aa2d3c8a837d8a320534a299b554e836
“Isaiah <92062860+isaiahroberts51@users.noreply.github.com>
2022-04-11 14:28:17 -0700

    F

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -51,7 +51,7 @@ class Menu extends Phaser.Scene {
             // hard mode
             game.settings ={
                 spaceshipSpeed: 4,
-                gameTimer: 40000
+                gameTimer: 45000
             }
             this.sound.play('sfx_select');
             this.scene.start('playScene');
